ff082838c35dcaa6ed52145ef5ce84af7e060249
Javi <jegido250@gmail.com>
2024-03-22 11:21:20 +0100

    notificaciones prueba

---
+++ app/src/main/java/com/example/apptareas/NotificationReceiver.kt
@@ -0,0 +1,50 @@
+package com.example.apptareas
+
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.apptareas.R
+import com.example.apptareas.activities.MainActivity
+
+class NotificationReceiver : BroadcastReceiver() {
+
+    companion object{
+        const val NOTIFICATION_ID = 1
+    }
+
+    override fun onReceive(context: Context, p1: Intent?) {
+        createSimpleNotification(context)
+    }
+
+    private fun createSimpleNotification(context: Context) {
+        Log.i("NOTI", "Se ha recibido una notificación")
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val flag = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, flag)
+
+        val notification = NotificationCompat.Builder(context, "prueba")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("My title")
+            .setContentText("Esto es un ejemplo <3")
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("Holita holi Holita holi ")
+            )
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(NOTIFICATION_ID, notification)
+    }
+}
